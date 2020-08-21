74211c0da58f5c6a9bc236455a6987a34a8bea9d
Ryosuke Uchitate <hello.ryosuke.u@gmail.com>
2020-08-21 16:00:00 +0900

    Add Spring Boot sample module

---
+++ spring-boot-sample/src/test/java/com/trianglesauce/springbootsample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trianglesauce.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
